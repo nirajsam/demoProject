b0c5da3195711850bc56b6d1737b97648c7744fd
nirajsam <nirajsam15@gmail.com>
2021-02-08 00:50:32 +0530

    video

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -1,8 +1,5 @@
 import {React} from 'react'
-
-
-
-
+import video from '../../public
 
 function MyWork() {
     
@@ -11,13 +8,22 @@ function MyWork() {
         <div>
         <div className="row" style={{paddingLeft:"10%",paddingRight:"10%"}}>
             <div className="col-lg-4 col-md-6 col-sm-1"  style={{marginTop:"5%"}}>
-                <iframe  height="215" src="https://www.youtube.com/embed/DUUR2KAP47A" frameborder="0" allowfullscreen ></iframe>
+                <video width="320" height="240" controls>
+                    <source src={video} type="video/mp4"/>
+                </video>
+                <p>subjective and objective test project</p>
             </div>
             <div className="col-lg-4 col-md-6 col-sm-1" style={{marginTop:"5%"}}>
-                <iframe  height="215" src="https://www.youtube.com/embed/DUUR2KAP47A" frameborder="0" allowfullscreen></iframe>
+                <video width="320" height="240" controls>
+                    <source src={video} type="video/mp4"/>
+                </video>
+                <p>Ekart project with paytm payment getway</p>
             </div>
             <div className="col-lg-4 col-md-6 col-sm-1" style={{marginTop:"5%"}}>
-                <iframe  height="215" src="https://www.youtube.com/embed/DUUR2KAP47A" frameborder="0" allowfullscreen></iframe>
+                <video width="320" height="240" controls>
+                    <source src={video} type="video/mp4"/>
+                </video>
+                <p>Whats app clone</p>
             </div>
         </div>
         </div>
